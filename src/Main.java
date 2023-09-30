36c139c315dae065f65c3007722e0aead6386169
elena.denoeva <denoeva.elena@skyeng.ru>
2023-09-30 14:25:51 +0300

    Домашняя работа 5.4 - задача из билета 11 про массивы книг и авторов (итоговое решение)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         // Исходный двухмерный массив по книгам и авторам
